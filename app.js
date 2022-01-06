b1b828e002812827e39b4f4682b782904d2c2308
H D Nidhishree <hdnidhi.shree@lowes.com>
2022-01-06 15:26:13 +0530

    Memory game - using JS

--- app.js
+++ app.js
@@ -26,12 +26,8 @@ document.addEventListener("DOMContentLoaded", () => {
 			img: "images/fries.png",
 		},
 		{
-			name: "cheeseburger",
-			img: "images/cheeseburger.png",
-		},
-		{
-			name: "ice-cream",
-			img: "images/ice-cream.png",
+			name: "hotdog",
+			img: "images/hotdog.png",
 		},
 		{
 			name: "pizza",
@@ -48,6 +44,10 @@ document.addEventListener("DOMContentLoaded", () => {
 		{
 			name: "milkshake",
 			img: "images/milkshake.png",
+		},
+		{
+			name: "fries",
+			img: "images/fries.png",
 		},
 	];
 
